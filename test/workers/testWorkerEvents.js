8e36bda788bdf0dc5f984e30eea5b962a8f686e3
eviltik <eviltik@gmail.com>
2017-11-15 21:47:10 +0100

    remove buggy tap test

--- test/workers/testWorkerEvents.js
+++ test/workers/testWorkerEvents.js
@@ -1,16 +1,5 @@
 const cluster = require('cluster');
 
-cluster.ec.onEvent('myEventForMasterAndMe', () => {
-    tap.test(common.me, common.testOptions, (t) => {
-        t.equal(
-            cluster.isSpawn,
-            true,
-            'event should be received by the spawn'
-        );
-        t.end();
-    });
-});
-
 setTimeout(()=>{
 
     cluster.ec.sendEvent('master:myEventToMaster');
