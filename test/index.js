5b27bc131d8205a9b0a2e3ff9a3c842937d92c41
eviltik <eviltik@gmail.com>
2018-03-04 20:27:18 +0100

    agaaaaaaaaain

--- test/index.js
+++ test/index.js
@@ -51,9 +51,10 @@ function testsStart() {
                     } else if (line === expectedLine3) {
                         t.same(line, expectedLine3, 'stdout line '+(stdout.length)+' should be "'+expectedLine3+'"');
                     } else {
-                        t.same(line, expectedLine1, 'stdout line -1 '+(stdout.length)+' should be "'+expectedLine1+'"');
-                        t.same(line, expectedLine2, 'stdout line  0 '+(stdout.length)+' should be "'+expectedLine2+'"');
-                        t.same(line, expectedLine3, 'stdout line +1 '+(stdout.length)+' should be "'+expectedLine3+'"');
+                        t.pass(line, expectedLine1, 'stdout line -1 '+(stdout.length)+' ===? "'+expectedLine1+'"');
+                        t.pass(line, expectedLine2, 'stdout line  0 '+(stdout.length)+' ===? "'+expectedLine2+'"');
+                        t.pass(line, expectedLine3, 'stdout line +1 '+(stdout.length)+' ===? "'+expectedLine3+'"');
+                        t.same(line, 'line -1, 0 or +1', 'stdout line '+(stdout.length)+' should match line -1, 0 or +1');
                     }
                 }
             });
